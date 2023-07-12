f5e5c62569cd54d404030fb2d61a51c4242f7665
Runtas, James <jamesruntas@cmail.carleton.ca>
2023-07-12 10:30:11 -0400

    javadoc

--- A1_james_runtas/PaintJob.java
+++ A1_james_runtas/PaintJob.java
@@ -1,31 +1,30 @@
 /**
  * 
- * 
- * 
+ * SYSC2004 Summer 2023 Assignment 1.
+ * PaintJob class. Contains 2 constructors, 2 getters/setters,
+ * as well as the calculate cost function.
  * 
  * @author James Runtas 
  * Student Number 101109175
  * @version Version 1.00 July 17th 2023
  * 
- * SYSC2004 Summer 2023 Assignment 1
  */
-
 import java.lang.Math;
-
 
 public class PaintJob{
 
-
     private static final float LITER_COVERS_SQUARE_FEET = 100;
-    public int numCans;
-    public float coatCost, paintNeeded;
-    private String projectName;
-    private float height, width, length, area;
-    private float budget;
-    private float canPrice;
+    //Static value for conversion of liters of paint to cover square foot area
+    
+    public int numCans; // number of cans 
+    public float coatCost, paintNeeded; // cost of current coat ($), paint needed (L)
+    private String projectName; //Name of project
+    private float height, width, length, area; //(feet) project area LxWxH and area
+    private float budget; //Project budget ($)
+    private float canPrice; //Cost of one paint can.
     
 
-
+    //Default constructor, setting all values to zero and/or null.
     public PaintJob(){
         numCans = 0;
         coatCost = 0;
@@ -39,6 +38,16 @@ public class PaintJob{
         canPrice = 0;
     }
 
+    /*
+     * Overloaded Constructor
+     * Creates a PaintJob object with the following attributes to be set (as well as calculate area)
+     * 
+     * @param iName
+     * @param iHeight
+     * @param iWidth
+     * @param iLength
+     * 
+     */
     public PaintJob(String iName, float iHeight, float iWidth, float iLength){
         numCans = 0;
         coatCost = 0;
@@ -55,13 +64,21 @@ public class PaintJob{
 
     }
 
+
+    /*
+     * Method to calculate the project cost for 1,2,3 coats of paint.
+     * 
+     * @param iCanPrice
+     * @param iBudget
+     */
     public void calculateCoatCoast(float iCanPrice, float iBudget){
         
+        //Loop 3 times, once per coat
         for (int i = 1; i<=3; i++){
             System.out.println("***********************\n");
             System.out.println("Project: " + projectName);
             // a Liter of paint covers 100 square feet
-	        paintNeeded = (area * i) / LITER_COVERS_SQUARE_FEET;
+	        paintNeeded = (area * i) / LITER_COVERS_SQUARE_FEET; // Paint needed for current coat number, including paint needed for previous coats
             System.out.println("Amount of paint needed for " + i +" coats: " + paintNeeded + "L");
 	        
             //number of 3.7 Liter cans needed to paint the wall
@@ -79,10 +96,18 @@ public class PaintJob{
 
     }
 
+
+    /*
+     * Setter function for the project budget
+     * @param iBudget
+     */
     public void setBudget(float iBudget) {
         this.budget = iBudget;
     }
 
+    /*
+     * Getter function for the project area
+     */
     public Float getArea() {
         return this.area;
     }
