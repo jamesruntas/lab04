bff1388067b9d927bc48822913cbdffb869c65bd
James <jamesruntas@cmail.carleton.ca>
2023-07-19 21:15:58 -0400

    init

---
+++ lab06_james_runtas/ToDo.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+/**
+ * 
+ * 
+ * 
+ * @author James Runtas 
+ * Student Number 101109175
+ * @version Version 1.03 July 19th 2023
+ * 
+ * SYSC2004 Summer 2023 Lab 6
+ */
+public class ToDo{
+
+    private final int count = 1;
+    private int itemNumber;
+    private int hours;
+    private int minutes;
+    private Boolean completed;
+    private String title;
+
+    public ToDo(){
+
+    }
+
+    public ToDo(String title, Boolean completed){
+
+    }
+
+    public void printItem(){
+
+    }
+
+    public void convertTime(String timeEnded){
+
+    }
+
+    public int getMinutes(){
+        return 0;
+    }
+
+    public void setMinutes(int minutes){
+
+    }
+
+    public void setHours(int hours){
+
+    }
+
+    public int getHours(){
+        return 0;
+    }
+
+    public String getTitle(){
+        return "";
+    }
+
+    protected void setCompleted(Boolean completed){
+        
+    }
+
+    public Boolean isCompleted(){
+        return false;
+    }
+
+    public void setTitle(String title){
+
+    }
+
+}
---
+++ lab06_james_runtas/main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+/**
+ * 
+ * 
+ * 
+ * @author James Runtas 
+ * Student Number 101109175
+ * @version Version 1.03 July 19th 2023
+ * 
+ * SYSC2004 Summer 2023 Lab 6
+ */
+public class main {
+
+    public static void Main(String[] args){
+
+    }
+    
+}
