a2882523f8d5d7b7d4b47e451448f7767ca7f921
James <jamesruntas@cmail.carleton.ca>
2023-07-19 22:10:51 -0400

    ver 10pm

--- lab06_james_runtas/ToDo.java
+++ lab06_james_runtas/ToDo.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
 import java.math.*;
 /**
  * 
@@ -14,7 +13,7 @@ import java.math.*;
  */
 public class ToDo{
 
-    private final int count = 1;
+    private static int count = 1;
     private int itemNumber;
     private int hours;
     private int minutes;
@@ -22,18 +21,49 @@ public class ToDo{
     private String title;
 
     public ToDo(){
-
+        this("No Item", false);
     }
 
     public ToDo(String title, Boolean completed){
-
+        count+=1;
+        this.itemNumber = count;
+        this.title = title;
+        this.completed = completed;
+        this.hours = 0;
+        this.minutes = 0;
     }
 
     public void printItem(){
+        if (this.completed){
+            System.out.println("Item " + itemNumber + ": " + title + " | Completed at " + hours + ":" + minutes);
+        }
+        else{
+            System.out.println("Item " + itemNumber + ": " + title + " | Not Completed");
+        }
 
     }
 
     public void convertTime(String timeEnded){
+        //int intTimeEnded = Integer.parseInt(timeEnded);
+        if (timeEnded.length() !=5){ //if time exceed 24hr format characters (4 + :)
+            hours = -1;
+            minutes = -1;
+        }
+        int iHours = Integer.parseInt(timeEnded.substring(0,1));
+        int iMinutes = Integer.parseInt(timeEnded.substring(3, 4));
+        
+        if (iHours > 23){
+            hours = -1;
+        }else{
+            hours = iHours;
+        }
+        
+        if (iMinutes>59){
+            minutes = -1;
+        }
+        else{
+            minutes = iMinutes;
+        }
 
     }
 
--- lab06_james_runtas/main.java
+++ lab06_james_runtas/main.java
@@ -1,7 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
 /**
  * 
  * 
@@ -14,8 +12,25 @@ import java.math.*;
  */
 public class main {
 
-    public static void Main(String[] args){
+    public static void main(String[] args){
 
+        LinkedList<ToDo> toDoList = new LinkedList<ToDo>();
+
+        ToDo toDo1 = new ToDo("Walk Dog", false);
+        ToDo toDo2 = new ToDo("Walk Cat", true);
+        ToDo toDo3 = new ToDo("Walk Hamster", false);
+        ToDo toDo4 = new ToDo("Walk Kids", false);
+
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+        toDoList.add(toDo4);
+
+        for (int i = 0; i < toDoList.size(); i++) {
+            ToDo toDo = toDoList.get(i);
+            
+            toDo.printItem();
+        }
     }
     
 }
