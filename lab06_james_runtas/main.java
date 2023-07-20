1d4ce36fcfbf583641919023814d59679fb9ae03
Runtas, James <jamesruntas@cmail.carleton.ca>
2023-07-20 12:51:24 -0400

    done

--- lab06_james_runtas/main.java
+++ lab06_james_runtas/main.java
@@ -1,7 +1,6 @@
-import java.io.*;
 import java.util.*;
 /**
- * 
+ * Main Method for creating a linkedList full of To Do objects.
  * 
  * 
  * @author James Runtas 
@@ -14,27 +13,29 @@ public class main {
 
     public static void main(String[] args){
 
-        LinkedList<ToDo> toDoList = new LinkedList<ToDo>();
+        LinkedList<ToDo> toDoList = new LinkedList<ToDo>(); //Create LinkedList of ToDo objects, a to-do list.
 
+        //Creating 4 To Do Objects
         ToDo toDo1 = new ToDo("Walk Dog", false);
         ToDo toDo2 = new ToDo("Walk Cat", true);
         ToDo toDo3 = new ToDo("Walk Hamster", false);
-        ToDo toDo4 = new ToDo("Walk Kids", false);
+        ToDo toDo4 = new ToDo("Walk Kids", true);  
 
-        toDo1.convertTime("00:00");
+        //Setting the time for the completed tasks
         toDo2.convertTime("22:21");
-        toDo3.convertTime("23:59");
         toDo4.convertTime("99:99");
 
+        //Add all objects to linkedList
         toDoList.add(toDo1);
         toDoList.add(toDo2);
         toDoList.add(toDo3);
         toDoList.add(toDo4);
 
+        //Loop through all 4 to do items in linked list
         for (int i = 0; i < toDoList.size(); i++) {
-            ToDo toDo = toDoList.get(i);
+            ToDo toDo = toDoList.get(i); // toDo is the current to do item in list
             
-            toDo.printItem();
+            toDo.printItem(); // print To Do item info
         }
     }
     
